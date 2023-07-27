f449607517b1d4bf7c3267d7cf9406f4f9558782
Filipe Simões Mota <45741228+filipemota130@users.noreply.github.com>
2023-07-27 12:52:43 -0300

    Add files via upload

---
+++ parser/Postfix.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Postfix {
+     public static void main(String[] args) throws IOException{
+        Parser parse = new Parser();
+        parse.expr(); System.out.write('\n');
+    }
+}
